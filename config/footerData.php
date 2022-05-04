a8a9376e947f2aebeb27c5683ed3ce8f504a6212
rowl <airowl.gasga@gmail.com>
2022-05-04 17:25:10 +0200

    aggiunto file sulla cartella config per footer data

---
+++ config/footerData.php
@@ -0,0 +1,175 @@
+<?php
+
+return [
+    dcComicsLinks => [
+        [
+            text => 'Characters',
+            url => '#',
+            current => false,
+        ],
+        [
+            text => 'Comics',
+            url => '#',
+            current => false,
+        ],
+        [
+            text => 'Movies',
+            url => '#',
+            current => false,
+        ],
+        [
+            text => 'Tv',
+            url => '#',
+            current => false,
+        ],
+        [
+            text => 'Games',
+            url => '#',
+            current => false,
+        ],
+        [
+            text => 'Collectibles',
+            url => '#',
+            current => false,
+        ],
+        [
+            text => 'Videos',
+            url => '#',
+            current => false,
+        ],
+        [
+            text => 'Fans',
+            url => '#',
+            current => false,
+        ],
+        [
+            text => 'News',
+            url => '#',
+            current => false,
+        ],
+        [
+            text => 'Shop',
+            url => '#',
+            current => false,
+        ]
+    ],
+    shopLinks => [
+        [
+            text => 'Shop DC',
+            url => '#',
+            current => false,
+        ],
+        [
+            text => 'Shop DC Collectibles',
+            url => '#',
+            current => false,
+        ]
+    ],
+    dcLinks => [
+        [
+            text => 'Term of Use',
+            url => '#',
+            current => false,
+        ],
+        [
+            text => 'Privacy policy (New)',
+            url => '#',
+            current => false,
+        ],
+        [
+            text => 'Ad Choices',
+            url => '#',
+            current => false,
+        ],
+        [
+            text => 'Advertising',
+            url => '#',
+            current => false,
+        ],
+        [
+            text => 'Jobs',
+            url => '#',
+            current => false,
+        ],
+        [
+            text => 'Subscriptions',
+            url => '#',
+            current => false,
+        ],
+        [
+            text => 'Talent Workshop',
+            url => '#',
+            current => false,
+        ],
+        [
+            text => 'CPSC Certificates',
+            url => '#',
+            current => false,
+        ],
+        [
+            text => 'Ratings',
+            url => '#',
+            current => false,
+        ],
+        [
+            text => 'Shop Help',
+            url => '#',
+            current => false,
+        ],
+        [
+            text => 'Contact Us',
+            url => '#',
+            current => false,
+        ]
+    ],
+    sitesLinks => [
+        [
+            text => 'DC',
+            url => '#',
+            current => false,
+        ],[
+            text => 'MAD Magazine',
+            url => '#',
+            current => false,
+        ],
+        [
+            text => 'DC Kids',
+            url => '#',
+            current => false,
+        ],
+        [
+            text => 'DC Universe',
+            url => '#',
+            current => false,
+        ],
+        [
+            text => 'DC Power Visa',
+            url => '#',
+            current => false,
+        ],
+    ],
+    iconLinks => [
+        [
+            image => 'footer-facebook.png',
+            url => '#'
+        ],
+        [
+            image => 'footer-twitter.png',
+            url => '#'
+        ],
+        [
+            image => 'footer-youtube.png',
+            url => '#'
+        ],
+        [
+            image => 'footer-pinterest.png',
+            url => '#'
+        ],
+        [
+            image => 'footer-periscope.png',
+            url => '#'
+        ]
+    ]
+];
+
+?>
