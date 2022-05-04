d01bd8c31d72cc84cf0b0749e3b427b48ad5cb30
rowl <airowl.gasga@gmail.com>
2022-05-04 17:18:19 +0200

    modifiche nei file app e header blade php

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,19 @@
-<h1>header</h1>
+<div id="header">
+    <div class="main-logo">
+        <img src="{{ asset('img/dc-logo.png') }}" alt="Logo DC">
+    </div>
+    <nav class="header-nav">
+        <ul>
+
+            @foreach ($headerData as $element)
+                <li>
+                    <a href="{{ $element['url'] }}">{{ $element['text'] }}</a>
+                </li>
+            @endforeach
+
+
+        </ul>
+    </nav>
+</div>
+
+{{--:class="{ active: link.current}"--}}
