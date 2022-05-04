50e612888d0dcfc1b1d7601b3f938ab04021ac7c
rowl <airowl.gasga@gmail.com>
2022-05-04 16:31:56 +0200

    creato file blade

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+</head>
+<body>
+
+    <header>
+        {{-- include serve per importare parti del body che devo rimanere fissi per tutte le pagine --}}
+        @include('partials.header')
+    </header>
+
+    <main>
+
+    </main>
+
+    <footer>
+        {{-- include serve per importare parti del body che devo rimanere fissi per tutte le pagine --}}
+        @include('partials.footer')
+    </footer>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//! PAGINA PRINCIPALE
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.app');
 });
