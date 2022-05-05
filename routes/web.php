65b6bb6d7b5ebbc9719efd5e3666f79374f1eeef
rowl <airowl.gasga@gmail.com>
2022-05-05 16:11:20 +0200

    creato una route per il blade single card

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,21 @@ use Illuminate\Support\Facades\Route;
 //! PAGINA PRINCIPALE
 Route::get('/', function () {
     return view('layouts.app');
-});
+})->name('guest-homepage');
 
 Route::get('/cards', function () {
     $comicsData = config('comicsData');
     $mainBottomData = config('mainBottomData');
     return view('guest.main-cards', [ 'comicsData' => $comicsData, 'mainBottomData' => $mainBottomData]);
+})->name('guest-cards');
+
+Route::get('/cards/{index}', function ($index) {
+    $comicsData = config('comicsData');
+    $mainBottomData = config('mainBottomData');
+    if (is_numeric($index) && $index >= 0 && $index < count($comicsData)) {
+        return view('guest.singleCard', [ 'comicsData' => $comicsData[$index], 'mainBottomData' => $mainBottomData]);
+    } else {
+        abort(404);
+    }
+
 });
