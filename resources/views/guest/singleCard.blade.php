e9c6c44322765b6cffd49c90566f3f8269888dc7
rowl <airowl.gasga@gmail.com>
2022-05-05 17:02:01 +0200

    aggiunto contenuto e stili per la single card page

--- resources/views/guest/singleCard.blade.php
+++ resources/views/guest/singleCard.blade.php
@@ -1,8 +1,33 @@
 @extends('layouts.app')
 
 @section('main-content')
-<div id="main-content">
-    <div id="jumbotron">
+    <div id="image-card">
+        <div class="container">
+            <img src="{{$comicsData['thumb']}}" alt="{{$comicsData['title']}}">
+        </div>
     </div>
-</div>
+    <div id="description-card" class="container">
+
+        <div class="heading-card">
+            <div class="col-left">
+                <h1>{{$comicsData['title']}}</h1>
+                <div class="label">
+                    <p>U.S. Price: <span>{{$comicsData['price']}}</span></p>
+                    <div class="label-left">
+                        <p>AVAILABLE</p>
+                        <span>Check Availability</span>
+                    </div>
+                </div>
+                <p class="description">{{$comicsData['description']}}</p>
+            </div>
+            <div class="col-right">
+                <img src="{{ asset('img/adv.jpg') }}" alt="">
+            </div>
+        </div>
+
+        <div class="sub-heading-card">
+            talenti e specifiche
+        </div>
+    </div>
+    @dump($comicsData)
 @endsection
