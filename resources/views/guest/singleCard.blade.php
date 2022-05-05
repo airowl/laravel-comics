ea8cd71c1cd4314ab7f842bbfd2ed3dc1a773bb4
rowl <airowl.gasga@gmail.com>
2022-05-05 17:37:44 +0200

    aggiunto altro contenuto nel single card page

--- resources/views/guest/singleCard.blade.php
+++ resources/views/guest/singleCard.blade.php
@@ -21,13 +21,68 @@
                 <p class="description">{{$comicsData['description']}}</p>
             </div>
             <div class="col-right">
-                <img src="{{ asset('img/adv.jpg') }}" alt="">
+                <img src="{{ asset('img/adv.jpg') }}" alt="adv image">
             </div>
         </div>
 
         <div class="sub-heading-card">
-            talenti e specifiche
+            <div class="container">
+                <div class="talent">
+                    <h1>Talent</h1>
+                    <ul>
+                        <li>
+                            <div class="title">
+                                Art by:
+                            </div>
+                            <div class="result">
+                                @foreach ($comicsData['artists'] as $element)
+                                    {{ $element.', ' }}
+                                @endforeach
+                            </div>
+                        </li>
+                        <li>
+                            <div class="title">
+                                Written by:
+                            </div>
+                            <div class="result">
+                                @foreach ($comicsData['writers'] as $element)
+                                    {{ $element.', ' }}
+                                @endforeach
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="specs">
+                    <h1>Specs</h1>
+                    <ul>
+                        <li>
+                            <div class="title">
+                                Series:
+                            </div>
+                            <div class="result">
+                                {{ strtoupper($comicsData['series']) }}
+                            </div>
+                        </li>
+                        <li>
+                            <div class="title">
+                                U.S. Price:
+                            </div>
+                            <div class="result">
+                                {{$comicsData['price']}}
+                            </div>
+                        </li>
+                        <li>
+                            <div class="title">
+                                On Sale Date:
+                            </div>
+                            <div class="result">
+                                {{$comicsData['sale_date']}}
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
     </div>
-    @dump($comicsData)
+    @dump($comicsData['artists'])
 @endsection
